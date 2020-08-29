d60f715a11d4522ac6a3b74d0f0a1eec1639d40a
Craig Smith <craig.smith@cumis.cooperators.ca>
2020-08-29 11:03:53 -0400

    added stubbed player search endpoint

---
+++ HockeyApi/src/HockeyApi/Features/Player/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HockeyApi.Features.Player
+{
+    public class PlayerController : Controller
+    {
+        [HttpGet("[controller]")]
+        public IActionResult Search(string q)
+        {
+            return Json($"query: {q}");
+        }
+    }
+}
